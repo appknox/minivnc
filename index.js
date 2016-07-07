5b170fd01a40d2d0d76e677c70f2f5575909d065
dhilipsiva <dhilipsiva@gmail.com>
2016-07-07 13:04:24 +0530

    Working refactor for redability and reliabultiy

--- index.js
+++ index.js
@@ -5,194 +5,209 @@
  *
  * Distributed under terms of the MIT license.
  */
-var WebSocketServer = require('ws').Server,
-	http = require('http'),
-	net = require('net')
+var WebSocketServer = require('ws').Server
+  , http = require('http')
+  , net = require('net')
+  , PORT = process.env.PORT || 8008
+  , server = http.createServer()
+  , wss = new WebSocketServer({
+    server: server
+  });
 
-var PORT = process.env.PORT || 9002
-var server = http.createServer()
-var wss = new WebSocketServer({
-	server: server
-})
+var messageHandler = function (ws, data, stream, touchStream, streamsCreatedCallback) {
+  console.log('received: %s', data);
+  var jsonData = JSON.parse(data);
+  if (jsonData.type === "subscribe") {
+    /*
+     * `jsonData.token` will have the token
+     http.get({
+     hostname: 'localhost',
+     port: 80,
+     path: '/',
+     }, (res) => {
+    // Do stuff with response
+    })
+    */
+    stream = net.connect({
+      // port: port
+      port: 30004
+    });
+    touchStream = net.connect({
+      // port: port
+      port: 31004
+    });
 
-wss.on('connection', function(ws) {
-	console.info('Got a client')
-	var stream, touchStream;
-	ws.on('message', function incoming(data) {
-		console.log('received: %s', data);
-		var jsonData = JSON.parse(data);
-		if (jsonData.type === "subscribe") {
-			/*
-			 * `jsonData.token` will have the token
-			 http.get({
-			 hostname: 'localhost',
-			 port: 80,
-			 path: '/',
-			 agent: false  // create a new agent just for this one request
-			 }, (res) => {
-			// Do stuff with response
-			})
-			*/
-			stream = net.connect({
-				// port: port
-				port: 30025
-			})
-			touchStream = net.connect({
-				// port: port
-				port: 31025
-			})
+    stream.on('error', function() {
+      console.error('CAP: Be sure to run `adb forward`')
+    })
+    touchStream.on('error', function() {
+      console.error('TOUCH: Be sure to run `adb forward`')
+    })
+    streamsCreatedCallback(stream, touchStream);
+    var readBannerBytes = 0;
+    var bannerLength = 2;
+    var readFrameBytes = 0;
+    var frameBodyLength = 0;
+    var frameBody = new Buffer(0);
+    var banner = {
+      version: 0,
+      length: 0,
+      pid: 0,
+      realWidth: 0,
+      realHeight: 0,
+      virtualWidth: 0,
+      virtualHeight: 0,
+      orientation: 0,
+      quirks: 0
+    };
 
-			stream.on('error', function() {
-				console.error('CAP: Be sure to run `adb forward`')
-			})
-			touchStream.on('error', function() {
-				console.error('TOUCH: Be sure to run `adb forward`')
-			})
+    function tryRead() {
+      for (var chunk; (chunk = stream.read());) {
+        // console.info('chunk(length=%d)', chunk.length);
+        for (var cursor = 0, len = chunk.length; cursor < len;) {
+          if (readBannerBytes < bannerLength) {
+            switch (readBannerBytes) {
+              case 0:
+                // version
+                banner.version = chunk[cursor];
+                break
+              case 1:
+                // length
+                banner.length = bannerLength = chunk[cursor];
+                break
+              case 2:
+              case 3:
+              case 4:
+              case 5:
+                // pid
+                banner.pid +=
+                  (chunk[cursor] << ((readBannerBytes - 2) * 8)) >>> 0;
+                break
+              case 6:
+              case 7:
+              case 8:
+              case 9:
+                // real width
+                banner.realWidth +=
+                  (chunk[cursor] << ((readBannerBytes - 6) * 8)) >>> 0;
+                break
+              case 10:
+              case 11:
+              case 12:
+              case 13:
+                // real height
+                banner.realHeight +=
+                  (chunk[cursor] << ((readBannerBytes - 10) * 8)) >>> 0;
+                break
+              case 14:
+              case 15:
+              case 16:
+              case 17:
+                // virtual width
+                banner.virtualWidth +=
+                  (chunk[cursor] << ((readBannerBytes - 14) * 8)) >>> 0;
+                break
+              case 18:
+              case 19:
+              case 20:
+              case 21:
+                // virtual height
+                banner.virtualHeight +=
+                  (chunk[cursor] << ((readBannerBytes - 18) * 8)) >>> 0;
+                break
+              case 22:
+                // orientation
+                banner.orientation += chunk[cursor] * 90;
+                break
+              case 23:
+                // quirks
+                banner.quirks = chunk[cursor];
+                break
+            }
 
-			var readBannerBytes = 0
-			var bannerLength = 2
-			var readFrameBytes = 0
-			var frameBodyLength = 0
-			var frameBody = new Buffer(0)
-			var banner = {
-				version: 0,
-				length: 0,
-				pid: 0,
-				realWidth: 0,
-				realHeight: 0,
-				virtualWidth: 0,
-				virtualHeight: 0,
-				orientation: 0,
-				quirks: 0
-			}
+            cursor += 1;
+            readBannerBytes += 1;
 
-			function tryRead() {
-				for (var chunk;
-					(chunk = stream.read());) {
-						console.info('chunk(length=%d)', chunk.length)
-						for (var cursor = 0, len = chunk.length; cursor < len;) {
-							if (readBannerBytes < bannerLength) {
-								switch (readBannerBytes) {
-									case 0:
-										// version
-										banner.version = chunk[cursor]
-										break
-									case 1:
-										// length
-										banner.length = bannerLength = chunk[cursor]
-										break
-									case 2:
-									case 3:
-									case 4:
-									case 5:
-										// pid
-										banner.pid +=
-											(chunk[cursor] << ((readBannerBytes - 2) * 8)) >>> 0
-										break
-									case 6:
-									case 7:
-									case 8:
-									case 9:
-										// real width
-										banner.realWidth +=
-											(chunk[cursor] << ((readBannerBytes - 6) * 8)) >>> 0
-										break
-									case 10:
-									case 11:
-									case 12:
-									case 13:
-										// real height
-										banner.realHeight +=
-											(chunk[cursor] << ((readBannerBytes - 10) * 8)) >>> 0
-										break
-									case 14:
-									case 15:
-									case 16:
-									case 17:
-										// virtual width
-										banner.virtualWidth +=
-											(chunk[cursor] << ((readBannerBytes - 14) * 8)) >>> 0
-										break
-									case 18:
-									case 19:
-									case 20:
-									case 21:
-										// virtual height
-										banner.virtualHeight +=
-											(chunk[cursor] << ((readBannerBytes - 18) * 8)) >>> 0
-										break
-									case 22:
-										// orientation
-										banner.orientation += chunk[cursor] * 90
-										break
-									case 23:
-										// quirks
-										banner.quirks = chunk[cursor]
-										break
-								}
+            if (readBannerBytes === bannerLength) {
+              // console.log('banner', banner);
+            }
+          } else if (readFrameBytes < 4) {
+            frameBodyLength += (chunk[cursor] << (readFrameBytes * 8)) >>> 0;
+            cursor += 1;
+            readFrameBytes += 1;
+            // console.info('headerbyte%d(val=%d)', readFrameBytes, frameBodyLength);
+          } else {
+            if (len - cursor >= frameBodyLength) {
+              // console.info('bodyfin(len=%d,cursor=%d)', frameBodyLength, cursor);
 
-								cursor += 1
-								readBannerBytes += 1
+              frameBody = Buffer.concat([
+                frameBody, chunk.slice(cursor, cursor + frameBodyLength)
+              ])
 
-								if (readBannerBytes === bannerLength) {
-									console.log('banner', banner)
-								}
-							} else if (readFrameBytes < 4) {
-								frameBodyLength += (chunk[cursor] << (readFrameBytes * 8)) >>> 0
-								cursor += 1
-								readFrameBytes += 1
-								console.info('headerbyte%d(val=%d)', readFrameBytes, frameBodyLength)
-							} else {
-								if (len - cursor >= frameBodyLength) {
-									console.info('bodyfin(len=%d,cursor=%d)', frameBodyLength, cursor)
+              // Sanity check for JPG header, only here for debugging purposes.
+              if (frameBody[0] !== 0xFF || frameBody[1] !== 0xD8) {
+                console.error(
+                  'Frame body does not start with JPG header', frameBody);
+              }
+              try {
+                ws.send(frameBody, {
+                  binary: true
+                });
 
-									frameBody = Buffer.concat([
-										frameBody, chunk.slice(cursor, cursor + frameBodyLength)
-									])
+              } catch (e) {
+                console.error("Error Sending Stuff" + e);
+              }
 
-									// Sanity check for JPG header, only here for debugging purposes.
-									if (frameBody[0] !== 0xFF || frameBody[1] !== 0xD8) {
-										console.error(
-											'Frame body does not start with JPG header', frameBody)
-									}
-									try {
-										ws.send(frameBody, {
-											binary: true
-										})
+              cursor += frameBodyLength;
+              frameBodyLength = readFrameBytes = 0;
+              frameBody = new Buffer(0);
+            } else {
+              // console.info('body(len=%d)', len - cursor);
 
-									} catch (e) {
-										console.error("Error Sending Stuff" + e)
-									}
+              frameBody = Buffer.concat([
+                frameBody, chunk.slice(cursor, len)
+              ]);
 
-									cursor += frameBodyLength
-									frameBodyLength = readFrameBytes = 0
-									frameBody = new Buffer(0)
-								} else {
-									console.info('body(len=%d)', len - cursor)
+              frameBodyLength -= len - cursor;
+              readFrameBytes += len - cursor;
+              cursor = len;
+            }
+          }
+        }
+      }
+    }
+    stream.on('readable', tryRead);
+    ws.on('close', function() {
+      console.info('Lost a client');
+      if (stream) {
+        stream.end();
+      }
+      if (touchStream) {
+        touchStream.end();
+      }
+    })
+  }
+  else if (jsonData.type === 'pointer'){
+    if (touchStream) {
+      touchStream.write(jsonData.data);
+    }
+    else{
+      console.log("touchStream is not defined!");
+    }
+  }
+}
 
-									frameBody = Buffer.concat([
-										frameBody, chunk.slice(cursor, len)
-									])
+var webSocketHandler = function(ws) {
+  console.info('Got a client');
+  var stream, touchStream;
+  ws.on('message', function(data) {
+    messageHandler(ws, data, stream, touchStream, function (newStream, newTouchStream) {
+      stream = newStream;
+      touchStream = newTouchStream;
+    });
+  });
+}
 
-									frameBodyLength -= len - cursor
-									readFrameBytes += len - cursor
-									cursor = len
-								}
-							}
-						}
-					}
-			}
-			stream.on('readable', tryRead)
-			ws.on('close', function() {
-				console.info('Lost a client')
-				// stream.end()
-				// touchStream.end()
-			})
-		}
-		else if (jsonData.type === 'pointer'){
-			touchStream.write(jsonData.data);
-		}
-	});
-})
-server.listen(PORT)
-console.info('Listening on port %d', PORT)
+wss.on('connection', webSocketHandler);
+server.listen(PORT);
+console.info('Listening on port %d', PORT);
